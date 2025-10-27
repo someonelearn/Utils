52f5716e4e2dd08b2a25e31144e0f2e5cf59a24b
Bypasser <127021221+someonelearn@users.noreply.github.com>
2025-10-27 22:25:22 +0700

    Update generation.py

--- training_utils/generation.py
+++ training_utils/generation.py
@@ -345,7 +345,7 @@ def default_trainer_init(
         model=model,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         args=sft_config,
         data_collator=data_collator,
         **kwargs,
@@ -516,7 +516,7 @@ class SFTPipelineTrainer:
         # 8) trainer init
         trainer_out = self.trainer_init_fn(
             model=model,
-            processing_class=tokenizer,
+            tokenizer=tokenizer,
             train_dataset=dataset_out.train_dataset,
             eval_dataset=dataset_out.eval_dataset,
             sft_config=sft_config,
